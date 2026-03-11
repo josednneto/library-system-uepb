dc285d258c563aaa84e6244a10db90ec8ab993fc
José Neto <josednn15@hotmail.com>
2026-03-11 11:11:21 -0300

    Create Livro.java

---
+++ Livro.java
@@ -0,0 +1,28 @@
+public class Livro {
+
+    private String titulo;
+    private String autor;
+    private boolean disponivel;
+
+    public Livro(String titulo, String autor) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.disponivel = true;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public boolean isDisponivel() {
+        return disponivel;
+    }
+
+    public void emprestar() {
+        disponivel = false;
+    }
+
+    public void devolver() {
+        disponivel = true;
+    }
+}
