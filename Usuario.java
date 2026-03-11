182ccd6ae6eca3b0bbf30c4b91ff469885910d5e
José Neto <josednn15@hotmail.com>
2026-03-11 11:05:16 -0300

    Update Usuario.java
    Atualização
    - adc prazoDevolucao

--- Usuario.java
+++ Usuario.java
@@ -1,17 +1,18 @@
 public class Usuario {
+
     protected String nome;
     protected String cpf;
-    protected String email;
 
-    public Usuario(String nome, String cpf, String email) {
+    public Usuario(String nome, String cpf) {
         this.nome = nome;
         this.cpf = cpf;
-        this.email = email;
     }
 
-    public void exibirDados() {
-        System.out.println("Nome: " + nome);
-        System.out.println("CPF: " + cpf);
-        System.out.println("Email: " + email);
+    public String getNome() {
+        return nome;
+    }
+
+    public int prazoDevolucao() {
+        return 7;
     }
 }
