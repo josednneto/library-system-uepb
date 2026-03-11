55bcc7214f75c93b9d1e1e68376f7112340a63f0
José Neto <josednn15@hotmail.com>
2026-03-11 10:57:38 -0300

    Create Usuario.java

---
+++ Usuario.java
@@ -0,0 +1,17 @@
+public class Usuario {
+    protected String nome;
+    protected String cpf;
+    protected String email;
+
+    public Usuario(String nome, String cpf, String email) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.email = email;
+    }
+
+    public void exibirDados() {
+        System.out.println("Nome: " + nome);
+        System.out.println("CPF: " + cpf);
+        System.out.println("Email: " + email);
+    }
+}
