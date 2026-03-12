c3855816f739ff4dcdbc3ab76f309c6f93c11758
José Neto <josednn15@hotmail.com>
2026-03-12 13:32:11 -0300

    Update Bibliotecario.java
    Atualização:
    - Controle de adicionar livro agora se encontra em biblioteca.
    - A Classe somente fará parte da Herança(Usuario -> Bibliotecario)

--- Bibliotecario.java
+++ Bibliotecario.java
@@ -3,8 +3,4 @@ public class Bibliotecario extends Usuario {
     public Bibliotecario(String nome, String cpf) {
         super(nome, cpf);
     }
-
-    public void cadastrarLivro(Livro livro, Biblioteca biblioteca) {
-        biblioteca.adicionarLivro(livro);
-    }
 }
