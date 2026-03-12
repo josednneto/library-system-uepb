3dd818bfb1da57e34782952b37d2c1a3ab0c7cbc
José Neto <josednn15@hotmail.com>
2026-03-12 13:38:17 -0300

    Update Biblioteca.java
    Atualização:
    - Agora, a classe passará a controlar tudo. (Adicionar Usuario/Livro, Empréstimo e Devolução.

--- Biblioteca.java
+++ Biblioteca.java
@@ -3,21 +3,52 @@ import java.util.List;
 
 public class Biblioteca {
 
-    private List<Livro> livros = new ArrayList<>();
     private List<Usuario> usuarios = new ArrayList<>();
+    private List<Livro> livros = new ArrayList<>();
+    private List<Emprestimo> emprestimos = new ArrayList<>();
 
-    public void adicionarLivro(Livro livro) {
-        livros.add(livro);
-    }
 
     public void adicionarUsuario(Usuario usuario) {
         usuarios.add(usuario);
     }
 
-    public void listarLivros() {
+    public void adicionarLivro(Livro livro) {
+        livros.add(livro);
+    }
 
-        for (Livro l : livros) {
-            System.out.println(l.getTitulo());
+    public void emprestarLivro(Usuario usuario, Livro livro) {
+
+        if (livro.isDisponivel()) {
+
+            livro.emprestar();
+
+            Emprestimo emprestimo = new Emprestimo(usuario, livro);
+            emprestimos.add(emprestimo);
+
+            System.out.println("Livro emprestado: " + livro.getTitulo());
+            System.out.println("Usuário: " + usuario.getNome());
+            System.out.println("Prazo: " + usuario.prazoDevolucao() + " dias");
+
+        } else {
+            System.out.println("Livro indisponível");
         }
+    }
+
+
+    public void devolverLivro(Livro livro) {
+
+        for (Emprestimo e : emprestimos) {
+
+            if (e.getLivro() == livro) {
+
+                livro.devolver();
+                emprestimos.remove(e);
+
+                System.out.println("Livro devolvido: " + livro.getTitulo());
+                return;
+            }
+        }
+
+        System.out.println("Esse livro não está emprestado.");
     }
 }
